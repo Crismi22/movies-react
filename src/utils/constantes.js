cc61d27b909383d04712257c5eea09bf459397da
Cristina Benitez <krisminitu@gmail.com>
2023-02-21 22:13:54 -0300

    update-crear rama produccion

---
+++ src/utils/constantes.js
@@ -0,0 +1,4 @@
+export const URL_BASE = 'https://api.themoviedb.org/3'
+export const API_KEY = 'd035f1f3d1355fbff53ec1e05939f654'
+
+// https://api.themoviedb.org/3/movie/550?api_key=d035f1f3d1355fbff53ec1e05939f654
