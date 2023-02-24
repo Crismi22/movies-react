8662d6b8a99b2bd8dc236758ed68092825497cc4
Cristina Benitez <krisminitu@gmail.com>
2023-02-24 00:06:27 -0300

    organizando maquetado

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1>prueba</h1>
+      <h1>aca iria home</h1>
+      <h2>dentro de home el carrusel</h2>
+      <h2>dos secciones con populares y mejores</h2>
+      <footer>aca iria componente footer</footer>
     </div>
   );
 }
