c5697fef2f1ca7e5203d98e6ded4f3d87532eac6
Cristina Benitez <krisminitu@gmail.com>
2023-03-04 17:35:17 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, { useState } from 'react';
 import { NavBar } from './components/NavBar';
+import { Home } from './components/Home';
 import './App.css';
 
 function App() {
+
+  // const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+
   return (
     <div className="App">
-      <NavBar />
-      <h1>aca iria home</h1>
+      <NavBar setSearchResults={setSearchResults} />
+      <Home searchResults={searchResults} /><h1>aca iria home</h1>
       <h2>dentro de home el carrusel</h2>
       <h2>dos secciones con populares y mejores</h2>
       <footer>aca iria componente footer</footer>
