cd28f4daebd38e7d2152d12f0eb43de62adae04b
Carolina <carolinawilberg@gmail.com>
2023-03-27 20:25:38 -0300

    agregue carruseles de pelis populares y mejor puntuadas en home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,57 @@
 import React from "react";
+import "./style.css";
 import HomeCarousel from "../../components/HomeCarousel";
-import { API_KEY } from "../../utils/constantes";
+import { API_KEY, URL_BASE } from "../../utils/constantes";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  const [movie, setMovie] = useState([]);
+  // API call para peliculas en cartelera
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`)
+    fetch(`${URL_BASE}/movie/now_playing?api_key=${API_KEY}`)
       .then((res) => res.json())
-      .then((data) => setMovie(data));
+      .then((data) => setNowPlayingMovies(data));
   }, []);
 
-  console.log(movie.results);
+  console.log(nowPlayingMovies.results);
+  // /API call para peliculas en cartelera
+
+  // API call para peliculas populares
+  const [popularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+    fetch(`${URL_BASE}/movie/popular?api_key=${API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => setPopularMovies(data));
+  }, []);
+
+  console.log(popularMovies.results);
+  // /API call para peliculas populares
+
+  // API call para peliculas mejor puntuadas
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  useEffect(() => {
+    fetch(`${URL_BASE}/movie/top_rated?api_key=${API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => setTopRatedMovies(data));
+  }, []);
+
+  console.log(topRatedMovies.results);
+  // /API call para peliculas mejor puntuadas
+
   return (
-    <div>
-      <HomeCarousel />
-    </div>
+    <>
+      <HomeCarousel movies={nowPlayingMovies} isNowPlaying={true} />
+      <div className="header-section">
+        <h2>Películas populares</h2>
+        <Link to="/populares">Ver más</Link>
+      </div>
+      <HomeCarousel movies={popularMovies} isNowPlaying={false} />
+      <div className="header-section">
+        <h2>Películas mejor puntuadas</h2>
+        <Link to="/mejores">Ver más</Link>
+      </div>
+      <HomeCarousel movies={topRatedMovies} isNowPlaying={false} />
+    </>
   );
 };
