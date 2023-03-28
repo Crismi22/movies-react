9db1d2305b347ee30e7a1d96853106d6f6e7e32b
Cristina Benitez <krisminitu@gmail.com>
2023-03-28 17:05:03 -0300

    pagina 404 estilada

--- src/pages/movies/Error404.js
+++ src/pages/movies/Error404.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import "./style404.css";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <div>
-      <h1>404 Error</h1>
-      
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </div>
+  	<div id="notfound">
+		<div className="notfound no-nav-footer">
+			<div className="notfound-404">
+				<h1>404</h1>
+				<h2>Page not found</h2>
+			</div>
+      <Link to={`/`}>Home</Link>
+		</div>
+	</div>
   );
 };
 
 export default Error404;
+ 
