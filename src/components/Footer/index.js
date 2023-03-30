c6e55a214f7f9bd8b2a6d71c208ad0183ed8beed
Cristina Benitez <krisminitu@gmail.com>
2023-03-30 20:12:31 -0300

    responsive de movies - carrousel y footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,48 +4,48 @@ import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white" style={{ marginTop: "50px" }}>
+    <footer className="bg-dark text-white" style={{ marginTop: "30px" }}>
       <div className="container py-3">
-      <span style={{ fontSize: '28px', fontFamily: 'Fasthand' }}>Realizado por Cris y Caro</span>
+        <div className="d-flex flex-column flex-md-row justify-content-center align-items-center">
+          <span style={{ fontSize: '28px', fontFamily: 'Fasthand' }}>Realizado por Cris y Caro</span>
 
-        <div className="d-flex justify-content-center" style={{ marginTop: "15px" }}>
-          <div className="d-flex align-items-center justify-content-between w-50 me-5" >
+          <div className="d-flex flex-wrap justify-content-center justify-content-md-between align-items-center mt-3 mt-md-0">
             <a
               href="https://www.linkedin.com/in/cristinabenitezcb/"
-              className="d-flex align-items-center text-decoration-none"
+              className="d-flex align-items-center text-decoration-none me-3"
             >
               <SiLinkedin size={24} className="me-2" /> Cristina Benitez
             </a>
             <a
               href="https://github.com/Crismi22"
-              className="d-flex align-items-center text-decoration-none"
+              className="d-flex align-items-center text-decoration-none me-3"
             >
               <SiGithub size={24} className="me-2" /> Crismi22
             </a>
-          </div>
-          <div className="d-flex align-items-center justify-content-between w-50">
             <a
               href="https://github.com/CarolinaWilberg"
-              className="d-flex align-items-center text-decoration-none"
+              className="d-flex align-items-center text-decoration-none me-3"
             >
               <SiGithub size={24} className="me-2" /> CarolinaWilberg
             </a>
             <a
               href="https://www.linkedin.com/in/carolina-wilberg"
-              className="d-flex align-items-center text-decoration-none"
+              className="d-flex align-items-center text-decoration-none me-3"
             >
               <SiLinkedin size={24} className="me-2" /> Carolina Wilberg
             </a>
           </div>
         </div>
+        <div className="d-flex justify-content-center mt-3">
+          <span>
+            {" "}
+            <i>
+              <FaRegCopyright />
+            </i>{" "}
+            Copyright 2023
+          </span>
+        </div>
       </div>
-      <span>
-        {" "}
-        <i>
-          <FaRegCopyright /> 
-        </i> 
-         Copyright 2023
-      </span>
     </footer>
   );
 };
